82fdda2fa9c265267b0fad62ef79bf06b750fffb
nnduc1994 <nnduc1994@gmail.com>
2015-11-05 22:44:46 +0200

    Create Database class

---
+++ FoodnStuff/Model/Database.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace FoodnStuff.Model
+{
+    public class Database
+    {
+        String connstr;
+        String projectPath;
+        OleDbConnection myConnection;
+        OleDbCommand myCommand;
+        OleDbDataReader myReader;
+
+        public Database()
+        {
+            projectPath = @"C:\Users\nguyngo2\Downloads\DatabaseExample";
+            connstr = "Provider = Microsoft.Jet.OLEDB.4.0;" +
+                 @"Data Source = " + projectPath + @"\Data\CustomerOrders2014.mdb;";
+            //OleDbConnection requires namespace System.Data.OleDb
+            myConnection = new OleDbConnection();
+            myConnection.ConnectionString = connstr;
+            myConnection.Open();
+        }
+        public OleDbConnection ReturnConnection()
+        {
+            return myConnection;
+        }
+
+        public void ExcuteNonQuery(string command)
+        {
+            myCommand = new OleDbCommand();
+            myCommand.CommandText = command;
+            myCommand.Connection = this.myConnection;
+            myCommand.ExecuteNonQuery();
+        }
+
+        public OleDbDataReader ExcuteQuery(string command)
+        {
+            myCommand = new OleDbCommand();
+            myCommand.CommandText = command;
+            myCommand.Connection = this.myConnection;
+            myReader = myCommand.ExecuteReader();
+            return myReader;
+        }
+
+        public void CloseConnection()
+        {
+            myConnection.Close();
+        }
+    }
+}
