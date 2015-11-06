7300048d78986f3e112782b76028e7535ade415f
nnduc1994 <nnduc1994@gmail.com>
2015-11-06 14:09:11 +0200

    Create Hashing class. Add example of how to use Password Hashing class.

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -5,15 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 using FoodnStuff;
 using System.Reflection;
-
+using FoodnStuff.Model;
 namespace TestApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string a = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace("\\bin\\Debug","");
-            Console.WriteLine(a);
+            //string a = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace("\\bin\\Debug","");
+            string Alreadypassword = PasswordHash.CreateHash("Hello this is a test");
+            bool check = PasswordHash.ValidatePassword("Hello this is a test",Alreadypassword);
+            Console.WriteLine(check);
             Console.ReadLine();
         }
     }
