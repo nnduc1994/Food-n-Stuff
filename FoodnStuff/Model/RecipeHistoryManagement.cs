bd9471fb9f8e40d15dc1e3f57e61f82b6a5b0875
Rostislav <rar95rylez@rambler.ru>
2015-11-20 14:06:18 +0300

    Added AddNewHR func

--- FoodnStuff/Model/RecipeHistoryManagement.cs
+++ FoodnStuff/Model/RecipeHistoryManagement.cs
@@ -7,5 +7,13 @@ namespace FoodnStuff.Model
 {
     public class RecipeHistoryManagement
     {
+        public void AddNewHR(string uid,string rid)
+        {
+            Database myDatabase = new Database();
+            myDatabase.ReturnConnection();
+            string command = "INSERT INTO CookingRecipeHistory (UserID,RecipeID,CookingDate) VALUES ('" + uid + "','" + rid + "','"   + DateTime.Now.Date.ToString() + "');";
+            myDatabase.ExcuteNonQuery(command);
+            
+        }
     }
 }
