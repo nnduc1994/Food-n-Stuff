8a3fa55e5ad2dcecd0d49d7f4d96a7968987577a
Rostislav <rar95rylez@rambler.ru>
2015-11-20 14:34:07 +0300

    Update SiteMaster Session code part

--- FoodnStuff/View/Shared/Site1.Master.cs
+++ FoodnStuff/View/Shared/Site1.Master.cs
@@ -4,14 +4,38 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FoodnStuff;
 
 namespace FoodnStuff.View.Shared
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
+        FoodnStuff.Model.UserManagement UM = new Model.UserManagement();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.Cookies["UserLogIn"] != null)
+            {
+                if (Request.Cookies["UserLogIn"]["UID"] != null)
+                {
+                    String ID = Request.Cookies["UserLogIn"]["UID"].ToString();
 
+                    Session["UID"] = ID;
+                    Session["Role"] = UM.getData("RoleId", ID);
+                    Session["Uname"] = UM.getData("UserName", ID);
+
+                    //Add here code to show Welcome message on panel
+                }
+            }
+            else
+            {
+                //Add here code to show Reg and login buttons on panel
+            }
+        }
+
+        public void LogOut() {
+            Session.Clear();
+            Response.Cookies.Clear();
         }
     }
 }
