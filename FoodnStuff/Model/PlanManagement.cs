3bfeb602c13613bec3da89ba55665d57ccbd1da6
nnduc1994 <nnduc1994@gmail.com>
2015-12-04 20:29:55 +0200

    add wish list management

---
+++ FoodnStuff/Model/PlanManagement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodnStuff.Model
+{
+    public class PlanManagement
+    {
+        //Add new row to Plan table
+        public static void AddPlan(int RecipeID, int UserID, DateTime CookingDate)
+        {
+
+
+        }
+
+        //Return a list of planRecipe Object belong to this specific UserID
+        public static List<PlanRecipe> GetPlanRecipe(int UserID) {
+
+            List<PlanRecipe> planRecipeList = new List<PlanRecipe>();
+            //Get data from DB, create new PlaneRecipe Object for each row, than add to list
+
+            return planRecipeList;
+        }
+
+        //Remove from the planID with ID of a row as an parameter
+        public static void RemoveFromPlan(int PlanID) { }
+    }
+
+    public class PlanRecipe{
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; }
+        public DateTime CookingDate { get; set; }
+        //use to remove from plan table
+        public int planID { get; set; }
+    }
+}
---
+++ FoodnStuff/Model/WishListManagement.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodnStuff.Model
+{
+    public class WishListManagement
+    {
+        //Add new row to ẂishList table
+        public static void AddToWishList(int RecipeID, int UserID)
+        {
+
+
+        }
+
+        //Return a list of planRecipe Object belong to this specific UserID
+        public static List<WishListRecipe> GetWishListRecipe(int UserID)
+        {
+
+            List<WishListRecipe> wishRecipeList = new List<WishListRecipe>();
+            //Get data from DB, create new PlaneRecipe Object for each row, than add to list
+
+            return wishRecipeList;
+        }
+
+        //use the ID of the wish in the DB to remove the row
+        public static void RemoveFromWishList(int WishListID) {
+
+        }
+    }
+
+    public class WishListRecipe
+    {
+        public int RecipeId { get; set; }
+        public string RecipeName { get; set; }
+        //The ID of the row in the wishList table, use to remove from wish list
+        public int WishID { get; set; }
+    }
+}
