e55721d91e764289120e5217fd4d2ec45237bc51
Rostislav <rar95rylez@rambler.ru>
2015-12-05 01:11:05 +0300

    Completed PlanManagement class

--- FoodnStuff/Model/PlanManagement.cs
+++ FoodnStuff/Model/PlanManagement.cs
@@ -10,21 +10,55 @@ namespace FoodnStuff.Model
         //Add new row to Plan table
         public static void AddPlan(int RecipeID, int UserID, DateTime CookingDate)
         {
-
-
+            Database myDatabase = new Database();
+            myDatabase.ReturnConnection();
+            string command = "INSERT INTO Plan (RecipeID,UserID,CookingDate) VALUES ('" + RecipeID + "','" + UserID + "','" + CookingDate + "');";
+            myDatabase.ExcuteNonQuery(command);
+            myDatabase.CloseConnection();
         }
 
         //Return a list of planRecipe Object belong to this specific UserID
         public static List<PlanRecipe> GetPlanRecipe(int UserID) {
 
             List<PlanRecipe> planRecipeList = new List<PlanRecipe>();
-            //Get data from DB, create new PlaneRecipe Object for each row, than add to list
+            Database myDatabase = new Database();
+            myDatabase.ReturnConnection();
+            string command = "SELECT * FROM Plan Where UserID=" + UserID + "";
+            var reader = myDatabase.ExcuteQuery(command);
+            bool notEOF = false;
+            notEOF = reader.Read();
+            while (notEOF)
+            {
+                planRecipeList.Add(new PlanRecipe(Convert.ToInt32(reader["RecipeID"]), "", Convert.ToDateTime(reader["CookingDate"]), Convert.ToInt32(reader["ID"])));
+                notEOF = reader.Read();
+            }
+
+            foreach (PlanRecipe wlr in planRecipeList)
+            {
+                command = "SELECT * FROM Recipe Where ID=" + wlr.RecipeId + "";
+                reader = myDatabase.ExcuteQuery(command);
+                notEOF = false;
+                notEOF = reader.Read();
+                while (notEOF)
+                {
+                    wlr.RecipeName = reader["Name"].ToString();
+                    notEOF = reader.Read();
+                }
+            }
+            myDatabase.CloseConnection();
 
             return planRecipeList;
         }
 
         //Remove from the planID with ID of a row as an parameter
-        public static void RemoveFromPlan(int PlanID) { }
+        public static void RemoveFromPlan(int PlanID) 
+        {
+            Database myDatabase = new Database();
+            myDatabase.ReturnConnection();
+            string command = "DELETE FROM Plan WHERE ID=" + PlanID + ";";
+            myDatabase.ExcuteNonQuery(command);
+            myDatabase.CloseConnection();
+        }
     }
 
     public class PlanRecipe{
@@ -33,5 +67,12 @@ namespace FoodnStuff.Model
         public DateTime CookingDate { get; set; }
         //use to remove from plan table
         public int planID { get; set; }
+        public PlanRecipe(int rId, string rN, DateTime cD, int pId)
+        {
+            RecipeId = rId;
+            RecipeName = rN;
+            CookingDate = cD;
+            planID = pId;
+        }
     }
 }
